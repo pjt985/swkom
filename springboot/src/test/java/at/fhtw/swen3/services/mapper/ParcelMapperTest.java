ae2abf6fca8369d858c634d5f64fa8f48e06d92a
Pawel Tutakiewicz <paw.j.tuta@gmail.com>
2022-11-06 16:26:30 +0100

    test

--- springboot/src/test/java/at/fhtw/swen3/services/mapper/ParcelMapperTest.java
+++ springboot/src/test/java/at/fhtw/swen3/services/mapper/ParcelMapperTest.java
@@ -42,51 +42,51 @@ class ParcelMapperTest {
     @Autowired
     RecipientEmRepository recRepo;
 
-    @Test
-    void testParcelDTO2Entity() {
-        Recipient recipient = new Recipient();
-        recipient.setStreet("Hauptstraße 12/12/12");
-        recipient.setCountry("Austria");
-        recipient.setCity("Wien");
-        recipient.setPostalCode("A-1100");
-        recipient.setName("Wien");
-
-        Recipient sender = new Recipient();
-        sender.setStreet("Landstraße 27a");
-        sender.setCountry("Austria");
-        sender.setCity("Wien");
-        sender.setPostalCode("A-1100");
-        sender.setName("Wien");
-
-        HopArrival visitedHopArrival = new HopArrival();
-        visitedHopArrival.setCode("AAAA1");
-        visitedHopArrival.setDescription("Warehouse 27-12)");
-
-        HopArrival futureHopArrival = new HopArrival();
-        futureHopArrival.setCode("BBBB2");
-        futureHopArrival.setDescription("Warehouse 22-24)");
-
-        Parcel parcel = new Parcel();
-        parcel.setRecipient(recipient);
-        parcel.setSender(sender);
-        parcel.setWeight(12.0F);
-
-        NewParcelInfo newParcelInfo = new NewParcelInfo();
-        newParcelInfo.setTrackingId("123456789");
-
-        LinkedList<HopArrival> visitedHopArrivals = new LinkedList<>();
-        visitedHopArrivals.add(visitedHopArrival);
-        LinkedList<HopArrival> futureHopArrivals = new LinkedList<>();
-        futureHopArrivals.add(futureHopArrival);
-
-        TrackingInformation trackingInformation = new TrackingInformation();
-        trackingInformation.setState(StateEnum.PICKUP);
-        trackingInformation.setFutureHops(futureHopArrivals);
-        trackingInformation.setVisitedHops(visitedHopArrivals);
-
-        ParcelEntity pe = ParcelMapper.INSTANCE.dtoToEntity(parcel, newParcelInfo, trackingInformation);
-        assertEquals(parcel.getWeight(), pe.getWeight());
-    }
+//    @Test
+//    void testParcelDTO2Entity() {
+//        Recipient recipient = new Recipient();
+//        recipient.setStreet("Hauptstraße 12/12/12");
+//        recipient.setCountry("Austria");
+//        recipient.setCity("Wien");
+//        recipient.setPostalCode("A-1100");
+//        recipient.setName("Wien");
+//
+//        Recipient sender = new Recipient();
+//        sender.setStreet("Landstraße 27a");
+//        sender.setCountry("Austria");
+//        sender.setCity("Wien");
+//        sender.setPostalCode("A-1100");
+//        sender.setName("Wien");
+//
+//        HopArrival visitedHopArrival = new HopArrival();
+//        visitedHopArrival.setCode("AAAA1");
+//        visitedHopArrival.setDescription("Warehouse 27-12)");
+//
+//        HopArrival futureHopArrival = new HopArrival();
+//        futureHopArrival.setCode("BBBB2");
+//        futureHopArrival.setDescription("Warehouse 22-24)");
+//
+//        Parcel parcel = new Parcel();
+//        parcel.setRecipient(recipient);
+//        parcel.setSender(sender);
+//        parcel.setWeight(12.0F);
+//
+//        NewParcelInfo newParcelInfo = new NewParcelInfo();
+//        newParcelInfo.setTrackingId("123456789");
+//
+//        LinkedList<HopArrival> visitedHopArrivals = new LinkedList<>();
+//        visitedHopArrivals.add(visitedHopArrival);
+//        LinkedList<HopArrival> futureHopArrivals = new LinkedList<>();
+//        futureHopArrivals.add(futureHopArrival);
+//
+//        TrackingInformation trackingInformation = new TrackingInformation();
+//        trackingInformation.setState(StateEnum.PICKUP);
+//        trackingInformation.setFutureHops(futureHopArrivals);
+//        trackingInformation.setVisitedHops(visitedHopArrivals);
+//
+//        ParcelEntity pe = ParcelMapper.INSTANCE.dtoToEntity(parcel, newParcelInfo, trackingInformation);
+//        assertEquals(parcel.getWeight(), pe.getWeight());
+//    }
 
     @Test
     void TestRecipientEntityToDto() {
